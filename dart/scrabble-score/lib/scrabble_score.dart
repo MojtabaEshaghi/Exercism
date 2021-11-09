b72210ad7ccb53d3631ed7b493fd3bd9b40bd5c7
mr.mojtaba <mr.mojtaba47@gmail.com>
2021-11-09 16:20:23 +0330

    add last project to git

--- dart/scrabble-score/lib/scrabble_score.dart
+++ dart/scrabble-score/lib/scrabble_score.dart
@@ -1,10 +1,44 @@
+int score(String input) {
+  int res = 0;
+  input.split('').forEach((element) {
+    res += setPoint(element.toLowerCase());
+  });
 
+  return res;
+}
 
-
-
-int result(String input) {
-
-
-
-
+int setPoint(String e) {
+  var point = 0;
+  if (e.contains("a") ||
+      e.contains("e") ||
+      e.contains("i") ||
+      e.contains("o") ||
+      e.contains("u") ||
+      e.contains("l") ||
+      e.contains("n") ||
+      e.contains("r") ||
+      e.contains("s") ||
+      e.contains("t")) {
+    point = 1;
+  } else if (e.contains("d") || e.contains("g")) {
+    point = 2;
+  } else if (e.contains("b") ||
+      e.contains("c") ||
+      e.contains("m") ||
+      e.contains("p")) {
+    point = 3;
+  } else if (e.contains("f") ||
+      e.contains("h") ||
+      e.contains("v") ||
+      e.contains("w") ||
+      e.contains("y")) {
+    point = 4;
+  } else if (e.contains("k")) {
+    point = 5;
+  } else if (e.contains("j") || e.contains("x")) {
+    point = 8;
+  } else if (e.contains("q") || e.contains("z")) {
+    point = 10;
+  }
+  return point;
 }
