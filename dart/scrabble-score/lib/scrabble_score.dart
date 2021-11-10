1fb0741b4726dd41b470f3e922454663c47ee4f3
mr.mojtaba <mr.mojtaba47@gmail.com>
2021-11-10 16:30:15 +0330

    grow up scrabble_score solution

--- dart/scrabble-score/lib/scrabble_score.dart
+++ dart/scrabble-score/lib/scrabble_score.dart
@@ -1,44 +1,35 @@
 int score(String input) {
-  int res = 0;
-  input.split('').forEach((element) {
-    res += setPoint(element.toLowerCase());
-  });
+  Map<String, int> letterValues = {
+    "a": 1,
+    "b": 3,
+    "c": 3,
+    "d": 2,
+    "e": 1,
+    "f": 4,
+    "g": 2,
+    "h": 4,
+    "i": 1,
+    "j": 8,
+    "k": 5,
+    "l": 1,
+    "m": 3,
+    "n": 1,
+    "o": 1,
+    "p": 3,
+    "q": 10,
+    "r": 1,
+    "s": 1,
+    "t": 1,
+    "u": 1,
+    "v": 4,
+    "w": 4,
+    "x": 8,
+    "y": 4,
+    "z": 10
+  };
 
-  return res;
-}
-
-int setPoint(String e) {
-  var point = 0;
-  if (e.contains("a") ||
-      e.contains("e") ||
-      e.contains("i") ||
-      e.contains("o") ||
-      e.contains("u") ||
-      e.contains("l") ||
-      e.contains("n") ||
-      e.contains("r") ||
-      e.contains("s") ||
-      e.contains("t")) {
-    point = 1;
-  } else if (e.contains("d") || e.contains("g")) {
-    point = 2;
-  } else if (e.contains("b") ||
-      e.contains("c") ||
-      e.contains("m") ||
-      e.contains("p")) {
-    point = 3;
-  } else if (e.contains("f") ||
-      e.contains("h") ||
-      e.contains("v") ||
-      e.contains("w") ||
-      e.contains("y")) {
-    point = 4;
-  } else if (e.contains("k")) {
-    point = 5;
-  } else if (e.contains("j") || e.contains("x")) {
-    point = 8;
-  } else if (e.contains("q") || e.contains("z")) {
-    point = 10;
-  }
-  return point;
+  return input
+      .toLowerCase()
+      .split('')
+      .fold(0, (score, element) => score + letterValues[element]!.toInt());
 }
